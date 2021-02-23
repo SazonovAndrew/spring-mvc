5cae3c57e3c0e7b68dbb3248d49e40a7d60da9da
Andrew <i.andrey.sazonov@yandex.ru>
2021-02-23 21:03:55 +0300

    Complete

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -2,17 +2,28 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarService;
+import web.model.Car;
+
+import java.util.List;
 
 @Controller
 public class CarController {
 
+    private final CarService carService;
+
     @Autowired
-    CarService carService;
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
-    @GetMapping
-    public void getCar(){
-
+    @GetMapping("/cars")
+    public String getCar(Model model, @RequestParam(defaultValue = "5") long count){
+        List<Car> carList = carService.getCarList(count);
+        model.addAttribute("carList", carList);
+        return "cars";
     }
 }

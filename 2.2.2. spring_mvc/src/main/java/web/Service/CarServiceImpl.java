372b147da0a11d10bb82f8a282775f4df9e1d8a2
Andrew <i.andrey.sazonov@yandex.ru>
2021-02-23 21:05:30 +0300

    Complete

---
+++ 2.2.2. spring_mvc/src/main/java/web/Service/CarServiceImpl.java
@@ -0,0 +1,34 @@
+package web.Service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService{
+
+    private long COUNT;
+    private List<Car> carList = new ArrayList<>();
+
+    {
+        carList.add(new Car(++COUNT, "LADA", 2021, 11000));
+        carList.add(new Car(++COUNT, "GAZ", 2010, 5000));
+        carList.add(new Car(++COUNT, "PAZ", 2015, 6000));
+        carList.add(new Car(++COUNT, "UAZ", 2021, 9000));
+        carList.add(new Car(++COUNT, "AURUS", 2021, 40000));
+        carList.add(new Car(++COUNT, "YO-MOBILE", 2009, 7000));
+    }
+
+    public List<Car> getCarList(long count){
+        if(count >= 5){
+            return carList;
+        }
+        return carList.stream()
+                .filter(c -> c.getId() <= count)
+                .collect(Collectors.toList());
+    }
+
+}
